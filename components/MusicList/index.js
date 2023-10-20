501c39e99cab88538b83c31fc4c7e1c5807a1160
Askar Kopbossynov <a.kopbossynov23@gmail.com>
2023-10-20 18:48:09 +0600

    feat: add music list component

---
+++ components/MusicList/index.js
@@ -0,0 +1,59 @@
+import React, { useState, useEffect } from 'react';
+
+const MusicList = () => {
+  // Sample list of songs
+  const songs = [
+    {
+      title: 'Song 1',
+      artist: 'Artist 1',
+      url: 'https://audiocdn.epidemicsound.com/ES_ITUNES/Y5lrB0_Calming%20Rain/ES_Calming%20Rain.mp3',
+    },
+    {
+      title: 'Song 2',
+      artist: 'Artist 2',
+      url: 'https://audiocdn.epidemicsound.com/ES_ITUNES/dALIml_Top%20Tier/ES_Top%20Tier.mp3',
+    },
+    // Add more songs as needed
+  ];
+
+  // State to manage the currently selected song
+  const [currentSong, setCurrentSong] = useState(null);
+
+  // Function to handle song selection
+  const handleSongSelect = (url) => {
+    setCurrentSong(url);
+  };
+
+  useEffect(() => {
+    // You can add additional logic here for handling audio playback, pausing, etc.
+  }, [currentSong]);
+
+  return (
+    <div className="bg-white p-4">
+      <h2 className="text-2xl font-bold mb-4">Music List</h2>
+      <ul>
+        {songs.map((song, index) => (
+          <li key={index} className="mb-4">
+            <div className="text-lg">{song.title}</div>
+            <div className="text-gray-600">{song.artist}</div>
+            <button
+              className="bg-blue-500 text-white px-4 py-2 rounded-md"
+              onClick={() => handleSongSelect(song.url)}
+            >
+              Play
+            </button>
+          </li>
+        ))}
+      </ul>
+      {currentSong }
+      {currentSong && (
+        <audio controls className="mt-4">
+          <source src={currentSong} type="audio/mpeg" />
+          Your browser does not support the audio element.
+        </audio>
+      )}
+    </div>
+  );
+};
+
+export default MusicList;

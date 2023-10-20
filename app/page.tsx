ff1a3699ccaa9edf2e71e012d6699e03f230e30f
Arkinov Abylay <arkinovabylay@gmail.com>
2023-10-20 20:15:26 +0600

    feat: slider

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
 "use client";
 import MusicList from '../components/MusicList/index';
 import Header from '../components/Header/index'
-
+import Slider from '@/components/Slider/Slider';
 export default function Home() {
   return (
     
     <div className="min-h-screen bg-gray-100">
       <Header />
+      <Slider/>
       <main>
         <div className="container mx-auto p-4">
           <MusicList />
